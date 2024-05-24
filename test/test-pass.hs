ad86579ff07f37118e10afe4452e71f48b4016d2
Rick Owens <rick@owensmurray.com>
2024-05-24 18:02:29 -0500

    refactor: Better no-op test output.

--- test/test-pass.hs
+++ test/test-pass.hs
@@ -1,4 +1,15 @@
 module Main (main) where
 
 main :: IO ()
-main = pure ()
+main =
+  putStrLn $
+    unlines
+      [ ""
+      , ""
+      , "    NB! We are _NOT_ really running the tests because the"
+      , "    `compile-elm` flag is not set. If you are sure that the"
+      , "    proper Elm tools are installed on the system, then you can"
+      , "    set the `compile-elm` flag and real tests will be run."
+      , ""
+      , ""
+      ]
